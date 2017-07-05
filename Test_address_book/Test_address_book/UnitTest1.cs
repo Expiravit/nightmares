7145ee6c7fdd47ba5ded3b2a8f6c9fa11f171529
Expiravit <avalongames@outlook.com>
2017-07-05 21:47:43 +0300

    Создан проект

---
+++ Test_address_book/Test_address_book/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_address_book
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
